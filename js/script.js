5ae2e3a8b263ce50173c8b23858c42b6623e53b8
Alan Ma <alan_ma@live.ca>
2017-08-23 17:13:54 -0400

    added many things

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
 // navbar transition
 $(function(){
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop(); // number of pixels scrolled
-        var scrollPoint = window.innerHeight / 5; // navbar changes after this point
-        if(scroll > scrollPoint){
-          $('.home-navbar').addClass('stick');
-          $('.nav-logo img').addClass('stick');
-        } else {
-          $('.home-navbar').removeClass('stick');
-          $('.nav-logo img').removeClass('stick');
-        }
-    });
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop(); // number of pixels scrolled
+    var scrollPoint = window.innerHeight / 5; // navbar changes after this point
+    if(scroll > scrollPoint){
+      $('.home-navbar').addClass('stick');
+      $('.nav-logo img').addClass('stick');
+    } else {
+      $('.home-navbar').removeClass('stick');
+      $('.nav-logo img').removeClass('stick');
+    }
+  });
+});
+
+var package = false;
+
+function showHiringPackage() {
+  package = true;
+  $('.hiring-package').css('visibility', 'visible');
+  $('.hiring-package').css('opacity', '1');
+  $('.hiring-package iframe').css('transform', 'translateY(0)');
+}
+
+function hideHiringPackage() {
+  $('.hiring-package').css('opacity', '0');
+  $('.hiring-package iframe').css('transform', 'translateY(100%)');
+  setTimeout(function() {
+    $('.hiring-package').css('visibility', 'hidden');
+    package = false;
+  }, 650);
+}
+
+$(document).keyup(function(event) {
+  if (event.keyCode === 27 && package) {
+    hideHiringPackage(); // esc key
+  }
 });
 
